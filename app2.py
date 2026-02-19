a91a7e6fea8ea12316da0c75091f2211623e0784
Sirawich <sirawich.aunasa@gmail.com>
2026-02-19 18:44:21 +0700

    final fix

--- app2.py
+++ app2.py
@@ -318,7 +318,9 @@ def export_excel(warehouse):
 # ---------------- RUN ---------------- #
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
 
 
 
