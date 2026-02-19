aef46fc50dba1f57c3b86f18342b154228075cb1
Sirawich <sirawich.aunasa@gmail.com>
2026-02-19 09:06:43 +0700

    remove app.run for render

--- app2.py
+++ app2.py
@@ -297,10 +297,6 @@ def export_excel(warehouse):
 
 # ---------------- RUN ---------------- #
 
-# ---------------- RUN ---------------- #
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
 
 
